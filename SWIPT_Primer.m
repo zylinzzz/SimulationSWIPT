ea8e6517d72278f831873410cd67803ce4899eba
Lin Zhenyi <Excellentlin.zy@gmail.com>
2018-03-26 15:30:26 +0800

    Problem 1 is OK

--- SWIPT_Primer.m
+++ SWIPT_Primer.m
@@ -3,7 +3,7 @@ Ps = 5e-3;  % The PSD constraint is 1 w
 Pt = 1;
 % Bs = 1*10^5; %The bandwidth is 100 KHz
 delta_sq = 1e-5; % The noise covariance
-beta = 0.7; % Variance
+beta = 0.5; % Variance
 M = 4; % Number of transmitter antennas
 N = 200; % Number of channels
 points = 13;
@@ -17,11 +17,13 @@ for d2 =  [7,8]
     Q1 = zeros(1,points);
     Q2 = zeros(1,points);
     R = zeros(1,points);
+    RN = zeros(1,points);
+    RF = zeros(1,points);
     N_E1 = zeros(size(Pc));
     N_E2 = N*ones(size(Pc));
     N_I1 = zeros(size(Pc));
     
-    for k = 1:7
+    for k = 1:points
         for i = 1:N
             h_sq(i) = norm(H(:,i),2).^2;
         end
@@ -36,7 +38,7 @@ for d2 =  [7,8]
             else
                 Q1(k) = Q1(k) + Ps * h_sq_sorted(i)/d1^2;
                 if Q1(k) > Pc(k)
-                    Q1(k) = Q1(k) - Ps * h_sq_sorted(i)/d1^1;
+                    Q1(k) = Q1(k) - Ps * h_sq_sorted(i)/d1^2;
                     N_E1(k) = i;
                     break
                 end
@@ -100,7 +102,6 @@ for d2 =  [7,8]
         for i = 1:N_E2(k)
             Pe(k) = Pe(k)+norm(v(:,i))^2;
         end
-%         Pe(k) = sum(norm(v(:,1:N_E(k)))^2);
         Pi = max(Pt - Pe(k),0);
         P_opt = WF_OPT(N,N_E1(k),Pi,Ps,h_sq_sorted,delta_sq);
         
@@ -108,32 +109,37 @@ for d2 =  [7,8]
         
         for i = N_E2(k)+1:N_I1(k)
             v(:,i) = sqrt(P_opt(i-N_E2(k)))* ...
-                conj(h_sorted(:,i))/ ...
+                (h_sorted(:,i))/ ...
                 (d1*norm(h_sorted(:,i),2));
         end
         
         for i = N_I1(k)+1:N
             v(:,i) = sqrt(P_opt(i-N_I1(k)))* ...
-                conj(h_sorted(:,i))/ ...
-                (d1*norm(h_sorted(:,i),2));
+                (h_sorted(:,i))/ ...
+                (d2*norm(h_sorted(:,i),2));
         end
         
 
         %% Calculate the sum rate
         for i = N_E1(k)+1:N_I1
-            R(k) = R(k) + log2(1+abs(h_sorted(:,i)' * v(:,i))^2/ ...
+            RN(k) = RN(k) + log2(1+abs(h_sorted(:,i)' * v(:,i))^2/ ...
                 (d1^2*delta_sq));
         end   
         for i = N_I1(k)+1:N
-            R(k) = R(k) + log2(1+abs(h_sorted(:,i)' * v(:,i))^2/ ...
+            RF(k) = RF(k) + log2(1+abs(h_sorted(:,i)' * v(:,i))^2/ ...
                 (d2^2*delta_sq));
         end
+        
+        R(k) = RN(k)+RF(k);
     end
     
     plot(Pc,R,'-*')
     hold on
+    plot(Pc,RN,'-*')
+    plot(Pc,RF,'-*')
     grid on
 end
-legend('R-NU','R-FU')
+legend('R(d1 = 6,d2 = 7)','R-NU(d1 = 6,d2 = 7)','R-FU(d1 = 6,d2 = 7)', ...
+    'R(d1 = 6,d2 = 8)','R-NU(d1 = 6,d2 = 8)','R-FU(d1 = 6,d2 = 8)')
 xlabel('Circuit Power Consumption (watta)')
 ylabel('Sum-Rate (bits per hertz)')
